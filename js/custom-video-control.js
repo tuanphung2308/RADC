66b2e8ab062ff2f3be6b7a11290afe3401a2c113
Tuan <tuanphung2308@gmail.com>
2020-11-29 19:06:22 +0700

    wangisnoob

---
+++ js/custom-video-control.js
@@ -0,0 +1,11 @@
+var media = document.querySelectorAll('video, audio');
+var rate = prompt('Speed? Currently playing back at ' + media[0].playbackRate + 'x.', '');
+Array.prototype.forEach.call(media, function (player) {
+    if (rate == null) {
+        return;
+    } else if (rate != 0) {
+        player.playbackRate = rate;
+    } else {
+        player.playbackRate = 1;
+    }
+});
